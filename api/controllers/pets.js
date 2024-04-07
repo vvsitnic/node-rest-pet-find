fcf38eaebb11cfe2a10c06a1776421a82a13bee0
vvsitnic <asleepcarpenterdev@gmail.com>
2024-04-07 21:59:51 +0300

    Changed all then() functions to async functions

--- api/controllers/pets.js
+++ api/controllers/pets.js
@@ -2,60 +2,60 @@ const mongoose = require('mongoose');
 const Pet = require('../models/pet.js');
 const fs = require('fs');
 
-const pets_on_map = (req, res, next) => {
+const pets_on_map = async (req, res, next) => {
 	// Fetch pets that are in configurable are
 	// localhost:3000/pets/on-map?n=&e=&s=&w=&f=
 	// TODO: change names of variables
-	const { n, e, s, w } = req.query;
-	const filter = req.query.f || ''; //.replace('userEmail', '')
+	try {
+		const { n, e, s, w } = req.query;
+		const filter = req.query.f || '';
 
-	Pet.find(
-		{
-			location: {
-				$geoWithin: {
-					$box: [
-						[+e, +n],
-						[+w, +s],
-					],
+		const petDocsArray = await Pet.find(
+			{
+				location: {
+					$geoWithin: {
+						$box: [
+							[+e, +n],
+							[+w, +s],
+						],
+					},
 				},
 			},
-		},
-		filter
-	)
-		.exec()
-		.then(result => {
-			console.log(result);
-			res.status(200).json(result);
-		})
-		.catch(err => next(err));
+			filter
+		).exec();
+
+		res.status(200).json(petDocsArray);
+	} catch (err) {
+		next(err);
+	}
 };
 
-const pets_nearby = (req, res, next) => {
+const pets_nearby = async (req, res, next) => {
 	// Fetch pets that are nearby, with the radius being a configurable parameter.
 	// localhost:3000/pets/nearby?lat=&lng=&d=&f=
-	const { lat, lng, d: distance } = req.query;
-	const filter = req.query.f || ''; //.replace('userEmail', '')
+	try {
+		const { lat, lng, d: distance } = req.query;
+		const filter = req.query.f || '';
 
-	Pet.find(
-		{
-			location: {
-				$near: {
-					$geometry: {
-						type: 'Point',
-						coordinates: [lng, lat],
+		const petDocsArray = await Pet.find(
+			{
+				location: {
+					$near: {
+						$geometry: {
+							type: 'Point',
+							coordinates: [lng, lat],
+						},
+						$maxDistance: distance,
 					},
-					$maxDistance: distance,
 				},
 			},
-		},
-		filter
-	)
-		.exec()
-		.then(result => {
-			console.log(result);
-			res.status(200).json(result);
-		})
-		.catch(err => next(err));
+			filter
+		).exec();
+
+		res.status(200).json(petDocsArray);
+	} catch (err) {
+		next(err);
+	}
 };
 
 const create_pet = async (req, res, next) => {
@@ -88,17 +88,17 @@ const create_pet = async (req, res, next) => {
 	}
 };
 
-const get_pet = (req, res, next) => {
+const get_pet = async (req, res, next) => {
 	// Get pet by id
 	// localhost:3000/pets/:id?f=
-	const id = req.params.id;
-	const filter = req.query.f || ''; //.replace('userEmail', '')
-	Pet.findById(id, filter)
-		.exec()
-		.then(doc => {
-			res.status(200).json(doc);
-		})
-		.catch(err => next(err));
+	try {
+		const id = req.params.id;
+		const filter = req.query.f || '';
+		const petDoc = await Pet.findById(id, filter);
+		res.status(200).json(petDoc);
+	} catch (err) {
+		next(err);
+	}
 };
 
 const get_pets_of_user = async (req, res, next) => {
@@ -108,7 +108,7 @@ const get_pets_of_user = async (req, res, next) => {
 		if (req.params.id !== req.userData.id)
 			return res.status(403).json({ message: 'Err' });
 
-		const filter = req.query.f || ''; //.replace('userEmail', '')
+		const filter = req.query.f || '';
 		const docs = await Pet.find({ userId: req.params.id }, filter).exec();
 		res.status(200).json(docs);
 	} catch (err) {
@@ -130,17 +130,21 @@ const get_pets_of_user = async (req, res, next) => {
 // 		});
 // }
 
-const delete_pet = (req, res, next) => {
+const delete_pet = async (req, res, next) => {
 	// Delete pet with specific id
-	Pet.deleteOne({ _id: req.params.id, userId: req.userData.id })
-		.exec()
-		.then(doc => {
-			if (doc.deletedCount === 0)
-				return res.status(403).json({ message: 'Pet not found' });
+	try {
+		const result = await Pet.deleteOne({
+			_id: req.params.id,
+			userId: req.userData.id,
+		}).exec();
 
-			res.status(200).json({ message: 'Pet deleted' });
-		})
-		.catch(err => next(err));
+		if (result.deletedCount === 0)
+			return res.status(403).json({ message: 'Pet not found' });
+
+		res.status(200).json({ message: 'Pet deleted' });
+	} catch (err) {
+		next(err);
+	}
 };
 
 module.exports = {
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,93 +5,92 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-const user_signup = (req, res, next) => {
-	User.findOne({ email: req.body.email })
-		.exec()
-		.then(user => {
-			if (user) {
-				return res.status(409).json({ message: 'Mail exists' });
-			} else {
-				bcrypt.hash(req.body.password, 10, (err, hash) => {
-					if (err) {
-						next(new Error(err)); //??????????????????
-					} else {
-						const user = new User({
-							_id: new mongoose.Types.ObjectId(),
-							email: req.body.email,
-							password: hash,
-							name: req.body.name,
-						});
-						user.save()
-							.then(result => {
-								console.log(result);
-								res.status(201).json({
-									message: 'User created',
-								});
-							})
-							.catch(err => next(err));
-					}
+const user_signup = async (req, res, next) => {
+	try {
+		const user = await User.findOne({ email: req.body.email }).exec();
+
+		if (user) {
+			return res.status(409).json({ message: 'Mail exists' });
+		}
+
+		bcrypt.hash(req.body.password, 10, async (err, hash) => {
+			try {
+				if (err) {
+					throw new Error(err);
+				}
+
+				const user = new User({
+					_id: new mongoose.Types.ObjectId(),
+					email: req.body.email,
+					password: hash,
+					name: req.body.name,
 				});
+				result = await user.save();
+
+				res.status(201).json({
+					message: 'User created',
+				});
+			} catch (err) {
+				throw err;
 			}
-		})
-		.catch(err => next(err));
+		});
+	} catch (err) {
+		next(err);
+	}
 };
 
-const user_login = (req, res, next) => {
-	User.findOne({ email: req.body.email })
-		.exec()
-		.then(user => {
-			if (!user) {
+const user_login = async (req, res, next) => {
+	try {
+		const user = User.findOne({ email: req.body.email }).exec();
+		if (!user) {
+			return res.status(401).json({
+				message: 'Auth failed',
+			});
+		}
+
+		bcrypt.compare(req.body.password, user.password, (err, response) => {
+			if (err) {
 				return res.status(401).json({
 					message: 'Auth failed',
 				});
 			}
-			bcrypt.compare(
-				req.body.password,
-				user.password,
-				(err, response) => {
-					if (err) {
-						return res.status(401).json({
-							message: 'Auth failed',
-						});
-					}
-					if (response) {
-						const token = jwt.sign(
-							{
-								email: user.email,
-								id: user.id,
-							},
-							process.env.ACCESSS_TOKE_SECRET,
-							{
-								expiresIn: '1h',
-							}
-						);
-						return res.status(201).json({
-							message: 'Auth successeful',
-							token: token,
-						});
-					}
 
-					res.status(401).json({
-						message: 'Auth failed',
-					});
-				}
-			);
-		})
-		.catch(err => next(err));
+			if (response) {
+				const token = jwt.sign(
+					{
+						email: user.email,
+						id: user.id,
+					},
+					process.env.ACCESSS_TOKE_SECRET,
+					{
+						expiresIn: '1h',
+					}
+				);
+				return res.status(201).json({
+					message: 'Auth successeful',
+					token: token,
+				});
+			}
+
+			res.status(401).json({
+				message: 'Auth failed',
+			});
+		});
+	} catch (error) {
+		next(err);
+	}
 };
 
-const delete_user = (req, res, next) => {
-	if (req.params.id !== req.userData.id)
-		return res.status(403).json({ message: 'Err' });
+const delete_user = async (req, res, next) => {
+	try {
+		if (req.params.id !== req.userData.id)
+			return res.status(403).json({ message: 'Err' });
 
-	User.deleteOne({ _id: req.params.id })
-		.exec()
-		.then(() => {
-			console.log(`User deleted`);
-			res.status(200).json({ message: 'User deleted' });
-		})
-		.catch(err => next(err));
+		await User.deleteOne({ _id: req.params.id }).exec();
+		res.status(200).json({ message: 'User deleted' });
+	} catch (err) {
+		next(err);
+	}
 };
 
 module.exports = {
