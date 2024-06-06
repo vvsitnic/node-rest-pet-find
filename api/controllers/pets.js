5eba216f96ce74e77045b4cc1d56e2edee581683
vvsitnic <asleepcarpenterdev@gmail.com>
2024-06-06 14:16:55 +0300

    changed names of query bariables in pets_on_map

--- api/controllers/pets.js
+++ api/controllers/pets.js
@@ -3,16 +3,16 @@ const Pet = require('../models/pet.js');
 const crypto = require('node:crypto');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const {
-	S3Client,
-	PutObjectCommand,
-	GetObjectCommand,
-	DeleteObjectCommand,
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
 } = require('@aws-sdk/client-s3');
 const sharp = require('sharp');
 
 require('dotenv').config();
 const randomImageName = (bytes = 32) =>
-	crypto.randomBytes(bytes).toString('hex');
+  crypto.randomBytes(bytes).toString('hex');
 
 const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
@@ -20,311 +20,311 @@ const bucketRegion = process.env.BUCKET_REGION;
 const s3 = new S3Client({ region: bucketRegion });
 
 const pets_on_map = async (req, res, next) => {
-	// Get pets in the area
-	// localhost:3000/pets/on-map?n=&e=&s=&w=&f=
-	// TODO: change names of variables
-	try {
-		// Find pets
-		const { n, e, s, w } = req.query;
-		const filter = req.query.f || '';
-		const petDocs = await Pet.find(
-			{
-				location: {
-					$geoWithin: {
-						$box: [
-							[+e, +n],
-							[+w, +s],
-						],
-					},
-				},
-			},
-			filter
-		).exec();
+  // Get pets in the area
+  // localhost:3000/pets/on-map?bllat=&bllng=&urlat=&urlng=&f=
+  // TODO: change names of variables
+  try {
+    // Find pets
+    const { bllat, bllng, urlat, urlng } = req.query;
+    const filter = req.query.f || '';
+    const petDocs = await Pet.find(
+      {
+        location: {
+          $geoWithin: {
+            $box: [
+              [+bllng, +bllat],
+              [+urlng, +urlat],
+            ],
+          },
+        },
+      },
+      filter
+    ).exec();
 
-		// Check if img urls are needed
-		if (petDocs.length !== 0 && petDocs[0].petImage) {
-			// Create img urls for each doc
-			for (const petDoc of petDocs) {
-				const getObjectParams = {
-					Bucket: bucketName,
-					Key: petDoc.petImage,
-				};
-				const command = new GetObjectCommand(getObjectParams);
-				const url = await getSignedUrl(s3, command, {
-					expiresIn: 3600,
-				});
-				petDoc.petImageUrl = url;
-			}
-		}
+    // Check if img urls are needed
+    if (petDocs.length !== 0 && petDocs[0].petImage) {
+      // Create img urls for each doc
+      for (const petDoc of petDocs) {
+        const getObjectParams = {
+          Bucket: bucketName,
+          Key: petDoc.petImage,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, {
+          expiresIn: 3600,
+        });
+        petDoc.petImageUrl = url;
+      }
+    }
 
-		res.status(200).json(petDocs);
-	} catch (err) {
-		next(err);
-	}
+    res.status(200).json(petDocs);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const pets_nearby = async (req, res, next) => {
-	// Get pets that are nearby
-	// localhost:3000/pets/nearby?lat=&lng=&d=&f=
-	try {
-		// Find pets
-		const { lat, lng, d: distance } = req.query;
-		const filter = req.query.f || '';
-		const petDocs = await Pet.find(
-			{
-				location: {
-					$near: {
-						$geometry: {
-							type: 'Point',
-							coordinates: [lng, lat],
-						},
-						$maxDistance: distance,
-					},
-				},
-			},
-			filter
-		).exec();
+  // Get pets that are nearby
+  // localhost:3000/pets/nearby?lat=&lng=&d=&f=
+  try {
+    // Find pets
+    const { lat, lng, d: distance } = req.query;
+    const filter = req.query.f || '';
+    const petDocs = await Pet.find(
+      {
+        location: {
+          $near: {
+            $geometry: {
+              type: 'Point',
+              coordinates: [lng, lat],
+            },
+            $maxDistance: distance,
+          },
+        },
+      },
+      filter
+    ).exec();
 
-		// Check if img urls are needed
-		if (petDocs.length !== 0 && petDocs[0].petImage) {
-			// Create img urls for each doc
-			for (const petDoc of petDocs) {
-				const getObjectParams = {
-					Bucket: bucketName,
-					Key: petDoc.petImage,
-				};
-				const command = new GetObjectCommand(getObjectParams);
-				const url = await getSignedUrl(s3, command, {
-					expiresIn: 3600,
-				});
-				petDoc.petImageUrl = url;
-			}
-		}
+    // Check if img urls are needed
+    if (petDocs.length !== 0 && petDocs[0].petImage) {
+      // Create img urls for each doc
+      for (const petDoc of petDocs) {
+        const getObjectParams = {
+          Bucket: bucketName,
+          Key: petDoc.petImage,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, {
+          expiresIn: 3600,
+        });
+        petDoc.petImageUrl = url;
+      }
+    }
 
-		res.status(200).json(petDocs);
-	} catch (err) {
-		next(err);
-	}
+    res.status(200).json(petDocs);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const create_pet = async (req, res, next) => {
-	// Create new pet
-	try {
-		// Edit img
-		const buffer = await sharp(req.file.buffer)
-			.resize(1920, 1080, { fit: 'cover' })
-			.toFormat('jpeg')
-			.jpeg({
-				quality: 75,
-				force: true,
-			})
-			.toBuffer();
+  // Create new pet
+  try {
+    // Edit img
+    const buffer = await sharp(req.file.buffer)
+      .resize(1920, 1080, { fit: 'cover' })
+      .toFormat('jpeg')
+      .jpeg({
+        quality: 75,
+        force: true,
+      })
+      .toBuffer();
 
-		// Create img
-		const imageName = randomImageName();
-		const params = {
-			Bucket: bucketName,
-			Key: imageName,
-			Body: buffer,
-			ContentType: req.file.mimetype,
-		};
+    // Create img
+    const imageName = randomImageName();
+    const params = {
+      Bucket: bucketName,
+      Key: imageName,
+      Body: buffer,
+      ContentType: req.file.mimetype,
+    };
 
-		const command = new PutObjectCommand(params);
+    const command = new PutObjectCommand(params);
 
-		// Create pet doc
-		const petData = JSON.parse(req.body.petData);
-		const pet = new Pet({
-			_id: new mongoose.Types.ObjectId(),
-			userId: req.userData.id,
-			petName: petData.petName,
-			description: petData.description,
-			details: petData.details,
-			petImage: imageName,
-			contacts: {
-				phone: petData.contacts.phone,
-				email: petData.contacts.email,
-			},
-			location: {
-				type: 'Point',
-				coordinates: [petData.coords.lng, petData.coords.lat],
-			},
-			dateLost: petData.dateLost,
-			reward: petData.reward,
-		});
-		const result = await pet.save();
+    // Create pet doc
+    const petData = JSON.parse(req.body.petData);
+    const pet = new Pet({
+      _id: new mongoose.Types.ObjectId(),
+      userId: req.userData.id,
+      petName: petData.petName,
+      description: petData.description,
+      details: petData.details,
+      petImage: imageName,
+      contacts: {
+        phone: petData.contacts.phone,
+        email: petData.contacts.email,
+      },
+      location: {
+        type: 'Point',
+        coordinates: [petData.coords.lng, petData.coords.lat],
+      },
+      dateLost: petData.dateLost,
+      reward: petData.reward,
+    });
+    const result = await pet.save();
 
-		// Upload img if everything is ok with doc
-		await s3.send(command);
+    // Upload img if everything is ok with doc
+    await s3.send(command);
 
-		res.status(201).json({ id: result.id });
-	} catch (err) {
-		next(err);
-	}
+    res.status(201).json({ id: result.id });
+  } catch (err) {
+    next(err);
+  }
 };
 
 const get_pet = async (req, res, next) => {
-	// Get pet
-	// localhost:3000/pets/:id?f=
-	try {
-		// Get pet
-		const id = req.params.id;
-		const filter = req.query.f || '';
-		const petDoc = await Pet.findById(id, filter);
+  // Get pet
+  // localhost:3000/pets/:id?f=
+  try {
+    // Get pet
+    const id = req.params.id;
+    const filter = req.query.f || '';
+    const petDoc = await Pet.findById(id, filter);
 
-		// Check if pet exists
-		if (!petDoc) return res.status(404).json({ message: 'Pet not found' });
+    // Check if pet exists
+    if (!petDoc) return res.status(404).json({ message: 'Pet not found' });
 
-		// Create pet img url
-		if (petDoc.petImage) {
-			const getObjectParams = {
-				Bucket: bucketName,
-				Key: petDoc.petImage,
-			};
-			const command = new GetObjectCommand(getObjectParams);
-			const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-			petDoc.petImageUrl = url;
-		}
+    // Create pet img url
+    if (petDoc.petImage) {
+      const getObjectParams = {
+        Bucket: bucketName,
+        Key: petDoc.petImage,
+      };
+      const command = new GetObjectCommand(getObjectParams);
+      const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+      petDoc.petImageUrl = url;
+    }
 
-		res.status(200).json(petDoc);
-	} catch (err) {
-		next(err);
-	}
+    res.status(200).json(petDoc);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const get_pets_of_user = async (req, res, next) => {
-	// Get pets of user
-	// localhost:3000/pets-of-user/:id?f=
-	try {
-		// Check if user is the one
-		if (req.params.id !== req.userData.id)
-			return res.status(404).json({ message: 'Pet not found' });
+  // Get pets of user
+  // localhost:3000/pets-of-user/:id?f=
+  try {
+    // Check if user is the one
+    if (req.params.id !== req.userData.id)
+      return res.status(404).json({ message: 'Pet not found' });
 
-		// Find docs
-		const filter = req.query.f || '';
-		const petDocs = await Pet.find({ userId: req.params.id }, filter).exec();
+    // Find docs
+    const filter = req.query.f || '';
+    const petDocs = await Pet.find({ userId: req.params.id }, filter).exec();
 
-		// Check if img urls are needed
-		if (petDocs.length !== 0 && petDocs[0].petImage) {
-			// Create img urls for each doc
-			for (const petDoc of petDocs) {
-				const getObjectParams = {
-					Bucket: bucketName,
-					Key: petDoc.petImage,
-				};
-				const command = new GetObjectCommand(getObjectParams);
-				const url = await getSignedUrl(s3, command, {
-					expiresIn: 3600,
-				});
-				petDoc.petImageUrl = url;
-			}
-		}
+    // Check if img urls are needed
+    if (petDocs.length !== 0 && petDocs[0].petImage) {
+      // Create img urls for each doc
+      for (const petDoc of petDocs) {
+        const getObjectParams = {
+          Bucket: bucketName,
+          Key: petDoc.petImage,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, {
+          expiresIn: 3600,
+        });
+        petDoc.petImageUrl = url;
+      }
+    }
 
-		res.status(200).json(petDocs);
-	} catch (err) {
-		next(err);
-	}
+    res.status(200).json(petDocs);
+  } catch (err) {
+    next(err);
+  }
 };
 
 // TODO: FIGUTE OUT HOW TO IMPLEMENT IMAGE THING
 const update_pet = async (req, res, next) => {
-	// Edit pet data
-	try {
-		// Check if pet exists and is created by the user
-		const petId = req.params.id;
-		const petDoc = await Pet.findOne(
-			{
-				_id: petId,
-				userId: req.userData.id,
-			},
-			'petImage'
-		);
+  // Edit pet data
+  try {
+    // Check if pet exists and is created by the user
+    const petId = req.params.id;
+    const petDoc = await Pet.findOne(
+      {
+        _id: petId,
+        userId: req.userData.id,
+      },
+      'petImage'
+    );
 
-		if (!petDoc) return res.status(404).json({ message: 'Pet not found' });
+    if (!petDoc) return res.status(404).json({ message: 'Pet not found' });
 
-		// Delete prev img
-		const deleteImgParams = {
-			Bucket: bucketName,
-			Key: petDoc.petImage,
-		};
-		const deleteImgCommand = new DeleteObjectCommand(deleteImgParams);
-		await s3.send(deleteImgCommand);
+    // Delete prev img
+    const deleteImgParams = {
+      Bucket: bucketName,
+      Key: petDoc.petImage,
+    };
+    const deleteImgCommand = new DeleteObjectCommand(deleteImgParams);
+    await s3.send(deleteImgCommand);
 
-		// Create new img
-		const buffer = await sharp(req.file.buffer)
-			.resize(1920, 1080, { fit: 'cover' })
-			.toBuffer();
+    // Create new img
+    const buffer = await sharp(req.file.buffer)
+      .resize(1920, 1080, { fit: 'cover' })
+      .toBuffer();
 
-		const imageName = randomImageName();
-		const createImgParams = {
-			Bucket: bucketName,
-			Key: imageName,
-			Body: buffer,
-			ContentType: req.file.mimetype,
-		};
+    const imageName = randomImageName();
+    const createImgParams = {
+      Bucket: bucketName,
+      Key: imageName,
+      Body: buffer,
+      ContentType: req.file.mimetype,
+    };
 
-		const createImgCommand = new PutObjectCommand(createImgParams);
-		await s3.send(createImgCommand);
+    const createImgCommand = new PutObjectCommand(createImgParams);
+    await s3.send(createImgCommand);
 
-		// Update pet
-		const update = {
-			petName: petData.petName,
-			description: petData.description,
-			details: petData.details,
-			petImage: imageName,
-			'contacts.phone': petData.contacts.phone,
-			'contacts.email': petData.contacts.email,
-			'location.coordinates': [petData.coords.lng, petData.coords.lat],
-			dateLost: petData.dateLost,
-			reward: petData.reward,
-		};
+    // Update pet
+    const update = {
+      petName: petData.petName,
+      description: petData.description,
+      details: petData.details,
+      petImage: imageName,
+      'contacts.phone': petData.contacts.phone,
+      'contacts.email': petData.contacts.email,
+      'location.coordinates': [petData.coords.lng, petData.coords.lat],
+      dateLost: petData.dateLost,
+      reward: petData.reward,
+    };
 
-		// Update pet
-		await Pet.updateOne({ _id: petId }, update).exec();
+    // Update pet
+    await Pet.updateOne({ _id: petId }, update).exec();
 
-		res.status(200).json({});
-	} catch (err) {
-		next(err);
-	}
+    res.status(200).json({});
+  } catch (err) {
+    next(err);
+  }
 };
 
 const delete_pet = async (req, res, next) => {
-	// Delete pet
-	try {
-		// Check if pet exists and is created by the user
-		const petId = req.params.id;
-		const petDoc = await Pet.findOne(
-			{
-				_id: petId,
-				userId: req.userData.id,
-			},
-			'petImage'
-		);
+  // Delete pet
+  try {
+    // Check if pet exists and is created by the user
+    const petId = req.params.id;
+    const petDoc = await Pet.findOne(
+      {
+        _id: petId,
+        userId: req.userData.id,
+      },
+      'petImage'
+    );
 
-		if (!petDoc) return res.status(404).json({ message: 'Pet not found' });
+    if (!petDoc) return res.status(404).json({ message: 'Pet not found' });
 
-		// Delete pet's img
-		const params = {
-			Bucket: bucketName,
-			Key: petDoc.petImage,
-		};
-		const command = new DeleteObjectCommand(params);
-		await s3.send(command);
+    // Delete pet's img
+    const params = {
+      Bucket: bucketName,
+      Key: petDoc.petImage,
+    };
+    const command = new DeleteObjectCommand(params);
+    await s3.send(command);
 
-		// Delete pet
-		await Pet.deleteOne({ _id: petId }).exec();
+    // Delete pet
+    await Pet.deleteOne({ _id: petId }).exec();
 
-		res.status(200).json({ message: 'Pet deleted' });
-	} catch (err) {
-		next(err);
-	}
+    res.status(200).json({ message: 'Pet deleted' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
-	pets_on_map,
-	pets_nearby,
-	create_pet,
-	get_pet,
-	delete_pet,
-	get_pets_of_user,
-	// update_pet,
+  pets_on_map,
+  pets_nearby,
+  create_pet,
+  get_pet,
+  delete_pet,
+  get_pets_of_user,
+  // update_pet,
 };
--- api/models/pet.js
+++ api/models/pet.js
@@ -1,49 +1,61 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const emailValidator = email => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
 const petSchema = new Schema(
-	{
-		_id: mongoose.Schema.Types.ObjectId,
-		userId: {
-			type: String,
-			required: true,
-		},
-		petName: {
-			type: String,
-			required: true,
-		},
-		description: {
-			type: String,
-			required: true,
-		},
-		details: String,
-		petImage: { type: String, required: true },
-		petImageUrl: { type: String, required: false },
-		contacts: {
-			phone: {
-				type: Number,
-				required: true,
-			},
-			email: String,
-		},
-		location: {
-			type: {
-				type: String,
-				enum: ['Point'],
-				required: true,
-			},
-			coordinates: {
-				type: [Number],
-				required: true,
-			},
-		},
-		dateLost: {
-			type: Number,
-			required: true,
-		},
-		reward: Boolean,
-	},
-	{ timestamps: true }
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    userId: {
+      type: String,
+      required: true,
+    },
+    petName: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    details: String,
+    petImage: { type: String, required: true },
+    petImageUrl: { type: String, required: false },
+    contacts: {
+      phone: {
+        type: Number,
+        required: true,
+      },
+      email: {
+        type: String,
+        validate: {
+          validator: emailValidator,
+          message: 'Not a valid email!',
+        },
+      },
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    dateLost: {
+      type: Number,
+      required: true,
+    },
+    reward: Boolean,
+  },
+  { timestamps: true }
 );
 
 petSchema.index({ location: '2dsphere' });
